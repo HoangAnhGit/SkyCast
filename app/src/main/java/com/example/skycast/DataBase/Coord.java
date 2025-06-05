db452b6f755209163329e4b4b814443f1c1f356d
HoangAnhGit <hoanganh2462004@gmail.com>
2025-06-05 21:17:58 +0700

    build model

---
+++ app/src/main/java/com/example/skycast/DataBase/Coord.java
@@ -0,0 +1,17 @@
+package com.example.skycast.DataBase;
+
+public class Coord {
+    private double lon;
+    private double lat;
+
+    public double getLon() { return lon; }
+    public double getLat() { return lat; }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+}
--- app/src/main/java/com/example/skycast/UI/CityFollow.java
+++ app/src/main/java/com/example/skycast/UI/CityFollow.java
@@ -17,10 +17,6 @@ public class CityFollow extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_city_follow);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
